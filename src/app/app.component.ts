e2aaad70264d73d6df5fdada1795d9df69694d00
sihem-hm <hammamisihem6@gmail.com>
2020-10-12 18:20:49 +0100

    Ractive form changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Reactivefroms';
-}
+  courseForm: FormGroup;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+  
+  onSubmit() {
+		// Since we have  access to the FormGroup instance we can directly output the same
+		console.log(this.courseForm);    
+	}
+
+	private initForm() {
+		this.courseForm = new FormGroup({
+		  'courseName': new FormControl(null, Validators.required),
+		  'courseDesc': new FormControl([Validators.required, Validators.minLength(100)]),
+		  'courseAmount': new FormControl(null)
+		});
+	
+}}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
